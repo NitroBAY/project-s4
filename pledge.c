b53becd436de69c49ba9cb0496856da09404515c
ilan <xd111@hotmail.fr>
2020-01-29 16:21:29 +0100

    start pledge

---
+++ pledge.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void pledge(size_t row, size_t col, int labyrinth[row][col]) {
+    for (size_t x = 0; x < col; x++) {
+        for (size_t y = 0; y < col; y++) {
+        
+        }
+    }
+}
+
