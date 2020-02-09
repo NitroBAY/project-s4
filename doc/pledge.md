1ab23079d70bcf7bc98a0675b0ddb401e7acae6d
ilan <xd111@hotmail.fr>
2020-02-09 18:07:02 +0100

    add doc for pledge

---
+++ doc/pledge.md
@@ -0,0 +1,2 @@
+## Directions
+Directions start at 0. -1 if robot goes right, +1 if it goes left. If direction modulo 4 is +2/-2 the robot goes to the south. +3 is like -1, -3 is like +1.
--- pledge.c
+++ pledge.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
 
 void pledge(size_t row, size_t col, int labyrinth[row][col]) {
-    for (size_t x = 0; x < col; x++) {
-        for (size_t y = 0; y < col; y++) {
-        
+    size_t robot_x = 0;
+    size_t robot_y = 0;
+    long directions = 0;
+
+    while (1) {
+        if (labyrinth[robot_y][robot_x] == 2) break;
+
+        if (directions == 0) {
+            if (labyrinth[robot_y + 1][robot_x] == 0) { // NO WALL IN FRONT OF ROBOT
+                robot_y++;
+            } else if (labyrinth[robot_y + 1][robot_x] == 1) { // WALL IN FRONT OF ROBOT
+                if (labyrinth[robot_y][robot_x - 1] == 0) { // WE CAN GO LEFT
+                    directions += 1;
+                    robot_x -= 1;
+                } else { // WE CAN ONLY GO RIGHT
+                    directions += 1;
+                    robot_x -= 1;
+                }
+            }
+        }
+        else {
+            if (labyrinth[robot_y][robot_x - 1] == 0) { // NO WALL IN THE WEST
+                if (labyrinth[robot_y + 1][robot_x] == 1) { // LET'S GO RIGHT TO KEEP WALL AT OUR RIGHT
+                    robot_y++;
+                    directions--;
+                } else {
+                    robot_x--;
+                }
+            } else {
+                if (labyrinth[robot_y - 1][robot_x] == 0) { // WE CAN GO LEFT (SOUTH)
+                    directions += 1;
+                    robot_y -= 1;
+                } else if (labyrinth[robot_y + 1][robot_x] == 0) { // WE CAN ONLY GO RIGHT (NORTH)
+                    directions += 1;
+                    robot_y += 1;
+                }
+            }
         }
     }
 }
