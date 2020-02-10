cba02e77fa338e139dca7df9e2d2dd2102c8cf50
Clotildelevou <43673552+Clotildelevou@users.noreply.github.com>
2020-02-10 17:46:20 +0100

    Update print_mat.h

--- print_mat/print_mat.h
+++ print_mat/print_mat.h
@@ -1,6 +1,6 @@
 #ifndef PRINT_MAT_H
 #define PRINT_MAT_H
 
-print_matrix(size_t row, size_t col, int laby[row][col], size_t robot_x, size_t robot_y);
+void print_matrix(size_t row, size_t col, int laby[row][col], size_t robot_x, size_t robot_y);
 
 #endif
